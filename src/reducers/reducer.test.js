1f7d2caf1e516273859b4a986e81eb38597040ce
joelpdurham <joeldur@gmail.com>
2020-02-13 14:30:20 -0800

    wrote failing snack reducer test

--- src/reducers/reducer.test.js
+++ src/reducers/reducer.test.js
@@ -1,5 +1,5 @@
 import { reducer } from './reducer';
-import { drinkCoffee } from '../actions/actions';
+import { drinkCoffee, eatSnack } from '../actions/actions';
 
 describe('REDUCER TESTS', () => {
   it('handle coffee case', () => {
@@ -11,12 +11,12 @@ describe('REDUCER TESTS', () => {
     expect(newState).toEqual({ coffees: 1 });
   });
 
-  // it.skip('handle snack case', () => {
-  //   const action = 'DRINK_COFFEE';
-  //   const intialState = { coffees: 0 };
+  it.skip('handle snack case', () => {
+    const action = eatSnack();
+    const intialState = { snacks: 0 };
 
-  //   const newState = reducer(intialState, action);
+    const newState = reducer(intialState, action);
 
-  //   expect(newState).toEqual({ coffees: 1 });
-  // });
+    expect(newState).toEqual({ snacks: 1 });
+  });
 });
